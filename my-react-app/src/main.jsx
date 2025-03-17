138b66d49d573556ed92e5d3b589606a7a7229a9
Buthmika <buthmikamenushan@gmail.com>
2025-03-17 08:36:30 +0530

    update

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Nav from './components/Navigation/Nav.jsx'
+import Content from './components/content/Content.jsx'
+import Content2 from './components/content/Content2.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    
+    <Nav/>
+    <Content></Content>
+    <Content2></Content2>
   </StrictMode>,
 )
