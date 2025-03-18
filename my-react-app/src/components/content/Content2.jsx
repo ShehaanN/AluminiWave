203895fe77ec2608871d57d3ab300f90c9d3c651
Buthmika <buthmikamenushan@gmail.com>
2025-03-18 19:50:05 +0530

    update

--- my-react-app/src/components/content/Content2.jsx
+++ my-react-app/src/components/content/Content2.jsx
@@ -44,7 +44,7 @@ function Content2() {
       </div>
       <div className="footer">
         <div className="part1">
-          <ul>
+          <ul className='a'>
             <li><a href="">Company</a></li>
             <li><a href="">About</a></li>
             <li><a href="">Carrers</a></li>
