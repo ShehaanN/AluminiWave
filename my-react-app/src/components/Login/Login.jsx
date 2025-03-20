e128da28305434b470be53545f518cbaed355008
Buthmika <buthmikamenushan@gmail.com>
2025-03-20 15:40:04 +0530

    Update Login.jsx

--- my-react-app/src/components/Login/Login.jsx
+++ my-react-app/src/components/Login/Login.jsx
@@ -13,33 +13,39 @@ function Login() {
             </div>
       </div>
       <div className="loginForm">
-      <div class="progress">
-            <div class="circle done">
-                  <span class="label">1</span>
-                  <span class="title">Personal</span>
+            <div class="progress">
+                  <div class="circle done">
+                        <span class="label">1</span>
+                        <span class="title">Personal</span>
                   </div>
                   <span class="bar done"></span>
                   <div class="circle done">
-                  <span class="label">2</span>
-                  <span class="title">Address</span>
+                        <span class="label">2</span>
+                        <span class="title">Address</span>
                   </div>
                   <span class="bar half"></span>
                   <div class="circle active">
-                  <span class="label">3</span>
-                  <span class="title">Payment</span>
+                        <span class="label">3</span>
+                        <span class="title">Payment</span>
                   </div>
                   <span class="bar"></span>
                   <div class="circle">
-                  <span class="label">4</span>
-                  <span class="title">Review</span>
+                        <span class="label">4</span>
+                        <span class="title">Review</span>
                   </div>
                   <span class="bar"></span>
                   <div class="circle">
-                  <span class="label">5</span>
-                  <span class="title">Finish</span>
+                        <span class="label">5</span>
+                        <span class="title">Finish</span>
                   </div>
             </div>
       </div>
+      <div className="loginInfo">
+             <b>Acount Creation</b><br />
+             Email or Phone <br /><input type="text" /><br />
+             Password <br /> <input type="password" />
+      </div>
+
     </div>
   )
 }
