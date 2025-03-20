bb252f48b0d99bb1ca51133a0ba04c874076e97f
ShehaanN <shehannadeesha2580@gmail.com>
2025-03-20 16:34:45 +0530

    Refactor main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Nav from './components/Navigation/Nav.jsx'
-import Content from './components/content/Content.jsx'
-import Content2 from './components/content/Content2.jsx'
-import Login from './components/Login/Login.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Nav from "./components/Navigation/Nav.jsx";
+import Content from "./components/content/Content.jsx";
+import Content2 from "./components/content/Content2.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    
-    {/* <Nav/>
+    <Nav />
     <Content></Content>
-    <Content2></Content2> */}
-    <Login/>
-  </StrictMode>,
-)
+    <Content2></Content2>
+  </StrictMode>
+);
