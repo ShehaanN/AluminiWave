07c052ff71bbaa3aa4d39944fa0f213d8e2be00b
Buthmika <buthmikamenushan@gmail.com>
2025-03-20 14:59:01 +0530

    update

---
+++ my-react-app/src/components/Login/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../../assets/logo.png'
+import './Login.css'
+
+function Login() {
+  return (
+    <div>
+      <div className="header">
+            <img src={logo}alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Login
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import Nav from './components/Navigation/Nav.jsx'
 import Content from './components/content/Content.jsx'
 import Content2 from './components/content/Content2.jsx'
+import Login from './components/Login/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,5 +13,6 @@ createRoot(document.getElementById('root')).render(
     {/* <Nav/>
     <Content></Content>
     <Content2></Content2> */}
+    <Login/>
   </StrictMode>,
 )
