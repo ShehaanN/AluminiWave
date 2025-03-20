4d3abc79c336c8d69da105159c0977756b56d35c
Buthmika <buthmikamenushan@gmail.com>
2025-03-20 15:27:19 +0530

    update

--- my-react-app/src/components/Login/Login.jsx
+++ my-react-app/src/components/Login/Login.jsx
@@ -12,6 +12,32 @@ function Login() {
                   <p>Connect, Share, and Grow with Fellow Graduates</p>
             </div>
       </div>
+      <div class="progress">
+            <div class="circle done">
+                  <span class="label">1</span>
+                  <span class="title">Personal</span>
+                  </div>
+                  <span class="bar done"></span>
+                  <div class="circle done">
+                  <span class="label">2</span>
+                  <span class="title">Address</span>
+                  </div>
+                  <span class="bar half"></span>
+                  <div class="circle active">
+                  <span class="label">3</span>
+                  <span class="title">Payment</span>
+                  </div>
+                  <span class="bar"></span>
+                  <div class="circle">
+                  <span class="label">4</span>
+                  <span class="title">Review</span>
+                  </div>
+                  <span class="bar"></span>
+                  <div class="circle">
+                  <span class="label">5</span>
+                  <span class="title">Finish</span>
+                  </div>
+            </div>
     </div>
   )
 }
