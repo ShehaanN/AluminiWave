071a716f17525714368fbf39679e8be1b292dcde
Buthmika <buthmikamenushan@gmail.com>
2025-03-25 18:32:55 +0530

    Update SignupStep2.jsx

--- src/components/Login/SignupStep2.jsx
+++ src/components/Login/SignupStep2.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../../assets/logo.png'
+import Loginstep from './Loginstep'
 
 function SignupStep2() {
   return (
@@ -10,6 +11,22 @@ function SignupStep2() {
                 <h1>Join Our Alumni Network</h1>
                 <p>Connect, Share, and Grow with Fellow Graduates</p>
               </div>
+            </div>
+            <div className="loginInfo2">
+                  <div className="steps1"><Loginstep></Loginstep></div>
+                  <p>Basic Profile Information</p>
+                  <div className="names">
+                        <div className="fname">
+                              First Name <br />
+                              <input type="text" name="" id="" placeholder='Enter First Name'/>
+                        </div>
+                        <div className="sname">
+                              Second Name <br />
+                              <input type="text" placeholder='Enter Second Name'/>
+                        </div>
+                  </div>
+                  <p>Professional Headline</p>
+                  <input type="text" placeholder='Enter Professional Headline'/>
             </div>
     </div>
   )
