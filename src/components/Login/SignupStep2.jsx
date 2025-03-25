b26122fd86263cd72528c1dda134cc2777693449
Buthmika <buthmikamenushan@gmail.com>
2025-03-25 18:05:51 +0530

    update

--- src/components/Login/SignupStep2.jsx
+++ src/components/Login/SignupStep2.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import logo from '../../assets/logo.png'
 
 function SignupStep2() {
   return (
     <div>
-      
+      <div className="header">
+              <img src={logo} alt="Logo" />
+              <div className="headerTopic">
+                <h1>Join Our Alumni Network</h1>
+                <p>Connect, Share, and Grow with Fellow Graduates</p>
+              </div>
+            </div>
     </div>
   )
 }
