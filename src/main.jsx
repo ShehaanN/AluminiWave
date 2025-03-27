54917d0070aa15caef20ac296de02f4ab7921b02
Buthmika <buthmikamenushan@gmail.com>
2025-03-27 19:08:44 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Content2 from "./components/content/Content2.jsx";
 import Login from "./components/Login/Login.jsx";
 import Loginstep from "./components/Login/Loginstep.jsx";
 import SignupStep2 from "./components/Login/SignupStep2.jsx";
+import SignupStep3 from "./components/Login/SignupStep3";
 
 
 createRoot(document.getElementById("root")).render(
@@ -16,7 +17,8 @@ createRoot(document.getElementById("root")).render(
     <Content></Content>
     <Content2></Content2> */}
     {/* <Login /> */}
-   {<SignupStep2></SignupStep2> }
+   {/* {<SignupStep2></SignupStep2> } */}
+   <SignupStep3></SignupStep3>
    
   </StrictMode>
 );
