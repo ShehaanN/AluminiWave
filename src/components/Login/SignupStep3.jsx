a38e244f6738699e8e39f7a8a010ff43b7c71694
Buthmika <buthmikamenushan@gmail.com>
2025-04-19 09:37:24 +0530

    Update SignupStep3.jsx

--- src/components/Login/SignupStep3.jsx
+++ src/components/Login/SignupStep3.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../../assets/logo.png';
+import './SignupStep3.css'
 
 function SignupStep3() {
   return (
@@ -7,9 +8,18 @@ function SignupStep3() {
       <div className="header">
                     <img src={logo} alt="Logo" />
                     <div className="headerTopic">
-                      <h1>Join Our Alumni Network</h1>
-                      <p>Connect, Share, and Grow with Fellow Graduates</p>
+                      <h1>Welcome to AluminiWave!</h1>
+                      <p className='is'>Your registration is complete</p>
                     </div>
+                    <div className="finalContent">
+                      <img src={logo} alt="" />
+                      <div className="complete">Registration Complete!</div><br />
+                      <p className='welcome'>Welcome back to the community!</p>
+                      <p className='exp'>Your experience in Technology can help shape the next generation.</p><br />
+                      <div className="we">✓ You can start connecting with other alumni</div>
+                      <button className='finalbutton'>Go to Dashboard</button>
+                    </div>
+                    <div className="need">Need help? Contact Support</div>
                     
     </div>
   )
