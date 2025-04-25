d44a7bb27f6a2e93c7f0a9a8d94bfb037accb328
ShehaanN <shehannadeesha2580@gmail.com>
2025-04-25 10:07:17 +0530

    Add Sidebar component

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,79 @@
+import logo from "../../assets/logo.png";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <aside className="w-64 bg-white border-r border-gray-200 fixed h-full">
+        <div className="p-6">
+          <img src={logo} alt="AlumniWave" className="w-full h-2/3 " />
+        </div>
+        <nav className="mt-6">
+          <a
+            href="#"
+            className="flex items-center px-6 py-3 text-gray-600 hover:bg-gray-50 "
+          >
+            <i className="fas fa-chart-line w-5 "></i>
+
+            <span className="ml-3 ">Dashboard</span>
+          </a>
+          <a
+            href="#"
+            className="flex items-center px-6 py-3 text-gray-600 hover:bg-gray-50"
+          >
+            <i className="fas fa-calendar w-5"></i>
+            <span className="ml-3">Events</span>
+          </a>
+          <a
+            href="#"
+            className="flex items-center px-6 py-3 text-gray-600 hover:bg-gray-50"
+          >
+            <i className="fas fa-users w-5"></i>
+            <span className="ml-3">Mentorship</span>
+          </a>
+          <a
+            href="#"
+            className="flex items-center px-6 py-3 text-gray-600 hover:bg-gray-50"
+          >
+            <i className="fas fa-briefcase w-5"></i>
+            <span className="ml-3">Job Portal</span>
+          </a>
+          <a
+            href="#"
+            className="flex items-center px-6 py-3 text-gray-600 hover:bg-gray-50"
+          >
+            <i className="fas fa-network-wired w-5"></i>
+            <span className="ml-3">Networking</span>
+          </a>
+          <div className="px-6 py-4 mt-6">
+            <p className="text-xs font-semibold text-gray-400 uppercase">
+              My Account
+            </p>
+          </div>
+          <a
+            href="#"
+            className="flex items-center px-6 py-3 text-gray-600 hover:bg-gray-50"
+          >
+            <i className="fas fa-user w-5"></i>
+            <span className="ml-3">Profile</span>
+          </a>
+          <a
+            href="#"
+            className="flex items-center px-6 py-3 text-gray-600 hover:bg-gray-50"
+          >
+            <i className="fas fa-award w-5"></i>
+            <span className="ml-3">Achievements</span>
+          </a>
+          <a
+            href="#"
+            className="flex items-center px-6 py-3 text-gray-600 hover:bg-gray-50"
+          >
+            <i className="fas fa-cog w-5"></i>
+            <span className="ml-3">Settings</span>
+          </a>
+        </nav>
+      </aside>
+    </div>
+  );
+};
+
+export default Sidebar;
