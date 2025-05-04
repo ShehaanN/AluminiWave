4b3c896a6dc1781720191747aff876220bfae8fa
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2025-05-04 10:23:37 +0530

    Update profiles

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,9 @@
 import React, { useState } from "react";
-import SideBar from './components/Sidebar';
-import MentorProfile from './pages/MentorProfile';
-import StudentProfile from './pages/StudentProfile';
+import MentorProfile from './componets/Profiles/MentorProfile';
+import StudentProfile from './components/Profiles/StudentProfile';
 
 function App() {
-  const [activeProfile, setActiveProfile] = useState('mentor');
-
-  const toggleProfile = () => {
-    setActiveProfile(activeProfile === 'mentor' ? 'student' : 'mentor');
-  };
-
-  return (
-    <div className="flex min-h-screen bg-gray-50">
-
-      <SideBar />
-
-      <div className="flex-1">
-        <div className="p-4 max-w-6xl mx-auto">
-
-          <button 
-            onClick={toggleProfile}
-            className="mb-4 bg-teal-500 text-white px-4 py-2 rounded-md"
-            >
-              Switch to {activeProfile === 'mentor' ? 'Student' : 'Mentor'} Profile
-          </button>
-
-          {activeProfile === 'mentor' ? <MentorProfile /> : <StudentProfile />}
-
-        </div>
-      </div>
-    </div>
-  );
+  return <StudentProfile />
 }
 
 export default App
